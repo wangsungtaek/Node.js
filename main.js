fd03a524a07f8dff133b5328f686c6866241298a
wangsungtaek <tjdxorv@naver.com>
2021-05-01 18:28:47 +0900

    디렉토리 읽기

--- main.js
+++ main.js
@@ -9,9 +9,16 @@ var app = http.createServer(function(request,response){
 
     if(pathname === '/'){
         if(queryData.id === undefined){
-            fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
+            fs.readdir('./data', function(error, filelist){
                 var title = 'Welcome';
                 var description = 'Hello, Node.js';
+                var list = "<ul>";
+                var i = 0
+                while(i<filelist.length){
+                    list = list + `<li><a href="./?id=${filelist[i]}">${filelist[i]}</a></li>`;
+                    i = i + 1;
+                }
+                list = list + "</ul>";
                 var template = `
                 <!doctype html>
                 <html>
@@ -21,11 +28,7 @@ var app = http.createServer(function(request,response){
                 </head>
                 <body>
                   <h1><a href="/">WEB</a></h1>
-                  <ol>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">JavaScript</a></li>
-                  </ol>
+                  ${list}
                   <h2>${title}</h2>
                   <p>${description}</p>
                 </body>
@@ -35,31 +38,36 @@ var app = http.createServer(function(request,response){
                 response.writeHead(200);
                 response.end(template);
             });
+
         } else {
             fs.readFile(`data/${queryData.id}`, 'utf-8', function(err, description){
-                var title = queryData.id;
-                var template = `
-                <!doctype html>
-                <html>
-                <head>
-                  <title>${title}</title>
-                  <meta charset="utf-8">
-                </head>
-                <body>
-                  <h1><a href="/">WEB</a></h1>
-                  <ol>
-                    <li><a href="/?id=HTML">HTML</a></li>
-                    <li><a href="/?id=CSS">CSS</a></li>
-                    <li><a href="/?id=JavaScript">JavaScript</a></li>
-                  </ol>
-                  <h2>${title}</h2>
-                  <p>${description}</p>
-                </body>
-                </html>
-                `;
+                fs.readdir('./data', function(error, filelist){
+                    var list = "<ul>";
+                    var i = 0
+                    while(i<filelist.length){
+                        list = list + `<li><a href="./?id=${filelist[i]}">${filelist[i]}</a></li>`;
+                        i = i + 1;
+                    }
+                    var title = queryData.id;
+                    var template = `
+                    <!doctype html>
+                    <html>
+                    <head>
+                      <title>${title}</title>
+                      <meta charset="utf-8">
+                    </head>
+                    <body>
+                      <h1><a href="/">WEB</a></h1>
+                      ${list}
+                      <h2>${title}</h2>
+                      <p>${description}</p>
+                    </body>
+                    </html>
+                    `;
 
-                response.writeHead(200);
-                response.end(template);
+                    response.writeHead(200);
+                    response.end(template);
+                });
             });
         }
     } else {
